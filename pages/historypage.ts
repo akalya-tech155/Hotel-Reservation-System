eb59c1acc01558af67d609f0afc4ce8e1ded16b0
akalya-tech155 <akalyavlab182@gmail.com>
2026-07-08 18:01:52 +0530

    created booking history file

---
+++ pages/historypage.ts
@@ -0,0 +1,37 @@
+/*
+
+Module 4
+Booking History
+
+Flow
+
+Login
+
+↓
+
+My Bookings
+
+↓
+
+Verify Latest Booking
+
+↓
+
+Verify
+
+Hotel Name
+
+Price
+
+Date
+
+Booking ID
+
+Status
+
+Assertions
+
+expect(bookingId).toContain("BK")
+expect(status).toHaveText("Confirmed")
+
+*/
