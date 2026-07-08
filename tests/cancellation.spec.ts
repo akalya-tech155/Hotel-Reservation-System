706e8fdb2723f5fa3d05e7a864d3c528a985067f
akalya-tech155 <akalyavlab182@gmail.com>
2026-07-08 18:09:06 +0530

    created cancellation-module file

---
+++ tests/cancellation.spec.ts
@@ -0,0 +1,36 @@
+/*
+
+Module 5
+Cancellation Module
+
+Flow
+
+Login
+
+↓
+
+Booking History
+
+↓
+
+Click Cancel
+
+↓
+
+Confirmation Popup
+
+↓
+
+Yes
+
+↓
+
+Verify Status
+
+Cancelled
+
+Assertions
+
+expect(status).toHaveText("Cancelled")
+
+*/
