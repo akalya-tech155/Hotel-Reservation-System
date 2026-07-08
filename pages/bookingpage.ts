45786178d0dcc857c4e49e7c02d6c6dca0a246a1
akalya-tech155 <akalyavlab182@gmail.com>
2026-07-08 17:40:15 +0530

    created booking file

---
+++ pages/bookingpage.ts
@@ -0,0 +1,44 @@
+/*
+
+Module 2
+Booking Module
+Flow
+
+Search Hotel
+
+↓
+
+Select Hotel
+
+↓
+
+Book Now
+
+↓
+
+Fill Guest Details
+
+Name
+Email
+Phone
+Address
+
+↓
+
+Continue
+
+Validations
+
+Required Name
+
+Required Email
+
+Invalid Email
+
+Empty Phone
+
+Assertions
+expect(nameTextbox).toHaveValue("Akash")
+expect(errorMessage).toContainText("Email is required")
+
+*/
